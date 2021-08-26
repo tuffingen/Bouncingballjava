9f71967085ab77a0ebd5341ebcdf642a9210b92e
tuffingen <60388902+tuffingen@users.noreply.github.com>
2021-08-26 13:39:29 +0200

    variabel och sout

---
+++ src/bouncing.java
@@ -0,0 +1,13 @@
+import java.util.Timer;
+
+public class bouncing {
+
+    public static void main(String[] args) {
+    double t = 0;
+    double y = 0;
+    double a = 9.82;
+
+
+        System.out.println("("+y+")" + "("+t+")");
+    }
+}

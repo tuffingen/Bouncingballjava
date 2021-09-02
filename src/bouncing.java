695e1cdb540c059bf1aac71ea516ef8f259d588e
tuffingen <60388902+tuffingen@users.noreply.github.com>
2021-09-02 13:31:09 +0200

    a

--- src/bouncing.java
+++ src/bouncing.java
@@ -5,9 +5,15 @@ public class bouncing {
     public static void main(String[] args) {
     double t = 0;
     double y = 0;
-    double a = 9.82;
+    double a = -9.82;
+    double v0 = 0;
+    double s = 1.5;
+    double v1 = v0 + a * t;
+
+        for(t>100){
+
+        }
 
 
-        System.out.println("("+y+")" + "("+t+")");
     }
 }

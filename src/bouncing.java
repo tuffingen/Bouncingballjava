288ea372aab898abec70f25832bb83d04e956c61
tuffingen <60388902+tuffingen@users.noreply.github.com>
2021-09-06 09:30:57 +0200

    a

--- src/bouncing.java
+++ src/bouncing.java
@@ -3,17 +3,26 @@ import java.util.Timer;
 public class bouncing {
 
     public static void main(String[] args) {
-    double t = 0;
-    double y = 0;
+
     double a = -9.82;
     double v0 = 0;
-    double s = 1.5;
-    double v1 = v0 + a * t;
+    double s = 100;
 
-        for(t>100){
 
+        for(int t = 0; t <= 100 ; t++) {
+            System.out.println(s + " " + t+" "+ v0);
+            if (s+v0 <= 0) {
+                // v1 = v0 + at
+                // s = s + v*deltaT
+                //deltaT = 1/v
+                double x = 1/v0;
+                v0 = -v0;
+                v0 = v0+(1-x)*a;
+                s += v0;
+            } else {
+                s += v0;
+                v0 = v0+a;
+            }
         }
-
-
     }
 }
